9474d25461f3a1a4bca03279fc2cb9a6f8031cb6
hongjiang <hongjiang@soyoung.com>
2018-04-18 14:48:38 +0800

    添加MVP基类

--- rr_lib/src/main/java/com/common/rr_lib/interceptor/LoggingInterceptor.java
+++ rr_lib/src/main/java/com/common/rr_lib/interceptor/LoggingInterceptor.java
@@ -1,5 +1,6 @@
 package com.common.rr_lib.interceptor;
 
+import com.common.rr_lib.BuildConfig;
 import com.common.rr_lib.Constant;
 
 import java.io.IOException;
@@ -13,14 +14,13 @@ public class LoggingInterceptor implements Interceptor {
     Request request = chain.request();
 
     long t1 = System.nanoTime();
-    if(Constant.DEBUG){
+    if(BuildConfig.DEBUG){
       System.out.println("t1 = " + t1 + "  url = " + request.url());
     }
     Response response = chain.proceed(request);
 
-    if(Constant.DEBUG) {
+    if(BuildConfig.DEBUG) {
       long t2 = System.nanoTime();
-
       System.out.println(" Received response for = " + response.request().url() + " time " +  (t2 - t1) / 1e6d );
     }
 
